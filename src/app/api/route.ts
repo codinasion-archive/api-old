1abc9901644f337d27196a8a81debbdca8056f3f
Harsh Raj <harshraj8843@gmail.com>
2023-04-24 09:07:21 +0530

    update

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,10 +4,7 @@ export async function GET(request: Request) {
   const res = {
     message: "Welcome to the Codinasion API 🤗",
     routes: [
-      {
-        path: "/contributors",
-        description: "List of all contributors",
-      },
+      
       {
         path: "/good-1st-issue",
         description: "List of good first issues",
@@ -20,10 +17,7 @@ export async function GET(request: Request) {
         path: "/good-1st-issue/labels/[label]",
         description: "List of good first issues with a specific label",
       },
-      {
-        path: "/humans.txt",
-        description: "Humans.txt file",
-      },
+      
       {
         path: "/og?path=[path]",
         description: "Open Graph images of Codinasion",
@@ -43,23 +37,8 @@ export async function GET(request: Request) {
       {
         path: "/program/tags/[tag]",
         description: "List of programs with a specific tag",
-      },
-      {
-        path: "/tools",
-        description: "List of @codinasion/tools",
-      },
-      {
-        path: "/tools/[slug]",
-        description: "Detail of a tool",
-      },
-      {
-        path: "/tools/tags",
-        description: "List of tool tags",
-      },
-      {
-        path: "/tools/tags/[tag]",
-        description: "List of tools with a specific tag",
-      },
+      }
+      
     ],
   };
 
